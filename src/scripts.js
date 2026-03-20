1aea96ed8558ffe381648a192290e2eba236d0e6
Pedro Henrique Guimarães <118992383+phthebest@users.noreply.github.com>
2026-03-20 13:17:22 -0300

    Add files via upload

--- src/scripts.js
+++ src/scripts.js
@@ -9,58 +9,85 @@ document.addEventListener("DOMContentLoaded", () => {
     const btnRecusar = document.getElementById("btn-recusar");
     const listaCapturados = document.getElementById("lista-capturados");
     const btnsNavegacao = document.querySelector(".btns-navegacao");
-    const btnLimpar = document.getElementById("btn-limpar")
-    pokemonImagem.src = "imagens/pikachu.gif";
+    const btnLimpar = document.getElementById("btn-pokedex");
+    const pokemonTipo = document.getElementById("pokemon-tipo");
 
     let pokemonAtual = null;
     let capturados = [];
 
     const dadosSalvos = localStorage.getItem("capturados");
-
+    
     if (dadosSalvos) {
         capturados = JSON.parse(dadosSalvos);
     }
 
     atualizarLista();
 
-    btnEncontrar.addEventListener("click", async () =>{
-        pokemonInfo.classList.add("hidden")
-        btnsNavegacao.classList.add("hidden")
-        mensagemLoading.classList.remove("hidden")
+    btnEncontrar.addEventListener("click", async () => {
+        pokemonInfo.classList.add("hidden");
+        btnsNavegacao.classList.add("hidden");
+        mensagemLoading.classList.remove("hidden");
 
-        const id = Math.floor(Math.random() *649)+ 1
+        const id = Math.floor(Math.random() * 649) + 1;
 
-        try{
+        try {
             const resposta = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`);
             const dados = await resposta.json();
-            const nivel = Math.floor(Math.random() *50) +1;
-            await new Promise(resolve => setTimeout(resolve, 5000));
 
-            const imagemAnimada = dados.sprites.versions["generation-v"]["black-white"].animated.front_default;
-            const tipos = dados.types.map(t => t.type.name).join(", ");
+            const nivel = Math.floor(Math.random() * 50) + 1;
+            await new Promise(resolve => setTimeout(resolve, 800));
+
+            const imagemAnimada = dados.sprites?.versions?.["generation-v"]?.["black-white"]?.animated?.front_default;
+            const tiposArray = dados.types.map(t => t.type.name);
+
+            mostrarTipo(tiposArray);
+
             const peso = dados.weight / 10;
             const altura = dados.height / 10;
-            
-    
-            pokemonAtual= {
+
+            pokemonAtual = {
                 nome: dados.name,
                 imagem: imagemAnimada || dados.sprites.front_default
             };
-            
-            pokemonImagem.src = pokemonAtual.imagem;
-            pokemonNome.textContent = `${pokemonAtual.nome.charAt(0).toUpperCase() + pokemonAtual.nome.slice(1)}      Lv${nivel}`;
-            pokemonDescricao.textContent = `Tipo: ${tipos} | Peso: ${peso}kg | Altura: ${altura}m`;
 
-            pokemonInfo.classList.remove("hidden");
-            btnsNavegacao.classList.remove("hidden");
-            mensagemLoading.classList.add("hidden");
-        }   catch (erro){
+            pokemonImagem.onload = () => {
+                pokemonInfo.classList.remove("hidden");
+                btnsNavegacao.classList.remove("hidden");
+                mensagemLoading.classList.add("hidden");
+                
+                pokemonInfo.classList.remove("animar"); // reset
+                void pokemonInfo.offsetWidth; // força reinício da animação
+                pokemonInfo.classList.add("animar");
+
+            };
+
+            pokemonImagem.src = pokemonAtual.imagem;
+
+
+            pokemonNome.textContent =
+                `${pokemonAtual.nome.charAt(0).toUpperCase() + pokemonAtual.nome.slice(1)} Lv${nivel}`;
+
+            pokemonDescricao.textContent =
+                `Peso: ${peso}kg | Altura: ${altura}m`;
+
+        } catch (erro) {
             mensagemLoading.classList.add("hidden");
             alert("Erro ao buscar pokemon");
         }
     });
 
-    btnCapturar.addEventListener("click", () =>{
+    function mostrarTipo(tiposArray) {
+        pokemonTipo.innerHTML = "";
+
+        tiposArray.forEach(tipo => {
+            const span = document.createElement("span");
+            span.textContent = tipo.toUpperCase();
+            span.classList.add("tipo", tipo);
+            pokemonTipo.appendChild(span);
+        });
+    }
+
+ btnCapturar.addEventListener("click", () =>{
         if(!pokemonAtual) return;
 
         capturados.push(pokemonAtual);
@@ -100,4 +127,3 @@ document.addEventListener("DOMContentLoaded", () => {
     }
 
 });
-
